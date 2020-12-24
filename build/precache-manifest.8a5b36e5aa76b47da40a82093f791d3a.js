1d4dda270d4b4d4981b16da39adae54a77a2125d
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2020-12-24 04:45:07 +0300

    Updates on config

---
+++ build/precache-manifest.8a5b36e5aa76b47da40a82093f791d3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95b4c385c1d517065ba637b50c254a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da5d90b174d349419974",
+    "url": "/static/css/main.1fe4be4a.chunk.css"
+  },
+  {
+    "revision": "c01a6f5231c992b748d5",
+    "url": "/static/js/2.8a596cfe.chunk.js"
+  },
+  {
+    "revision": "da5d90b174d349419974",
+    "url": "/static/js/main.c3a557e9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
